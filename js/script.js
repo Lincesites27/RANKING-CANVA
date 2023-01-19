0cb07ae4b2089703f9803947d443ae4607c757f6
lincesites27 <contato@lincesites.com.br>
2023-01-19 17:34:19 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,38 @@
+var dataRanking = document.querySelector("#dataRanking")
+var primeiroColocado = document.querySelector("#primeiroColocado")
+var segundoColocado = document.querySelector("#segundoColocado")
+var terceiroColocado = document.querySelector("#terceiroColocado")
+var dataPubli = document.querySelector("#dataPubli")
+var nome1 = document.querySelector("#nome1")
+var nome2 = document.querySelector("#nome2")
+var nome3 = document.querySelector("#nome3")
+var downloadButton = document.getElementById("#downloadButton");
+var divToDownload = document.getElementById("#divToDownload");
+
+  
+
+dataRanking.addEventListener("input", ()=>{
+    dataPubli.textContent = dataRanking.value
+})
+
+primeiroColocado.addEventListener("input", ()=>{
+    nome1.textContent = primeiroColocado.value
+})
+
+segundoColocado.addEventListener("input", ()=>{
+    nome2.textContent = segundoColocado.value
+})
+
+terceiroColocado.addEventListener("input", ()=>{
+    nome3.textContent = terceiroColocado.value
+})
+
+downloadButton.addEventListener("click", function() {
+    html2canvas(divToDownload).then(function(canvas) {
+      var data = canvas.toDataURL();
+      var downloadLink = document.createElement("a");
+      downloadLink.href = data;
+      downloadLink.download = "imagem.png";
+      downloadLink.click();
+    });
+  });
